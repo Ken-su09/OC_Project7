045232f3fed03218391b0dd915f3fbd02dba9287
Ken Suon <ken.su_dev@hotmail.com>
2022-05-28 13:17:20 +0200

    Unit tests for Places and Restaurants repositories

--- app/src/test/java/com/suonk/oc_project7/repositories/restaurants/RestaurantsRepositoryImplTest.java
+++ app/src/test/java/com/suonk/oc_project7/repositories/restaurants/RestaurantsRepositoryImplTest.java
@@ -1,4 +1,4 @@
-package com.suonk.oc_project7.repositories.places;
+package com.suonk.oc_project7.repositories.restaurants;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
@@ -8,14 +8,13 @@ import android.os.Looper;
 import androidx.annotation.NonNull;
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
 import com.suonk.oc_project7.api.PlacesApiService;
 import com.suonk.oc_project7.model.data.places.Place;
 import com.suonk.oc_project7.model.data.restaurant.Restaurant;
-import com.suonk.oc_project7.repositories.current_location.CurrentLocationRepository;
+import com.suonk.oc_project7.repositories.places.PlacesRepository;
+import com.suonk.oc_project7.repositories.places.PlacesRepositoryImpl;
 import com.suonk.oc_project7.utils.TestUtils;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -25,7 +24,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
-public class PlacesRepositoryImplTest {
+public class RestaurantsRepositoryImplTest {
 
     @Rule
     public final InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
@@ -35,24 +34,24 @@ public class PlacesRepositoryImplTest {
 
     private final String defaultLocation = "48.9575329%2C2.5803872";
 
-    private PlacesRepository placesRepository;
+    private RestaurantsRepository restaurantsRepository;
 
     @Before
     public void setUp() {
-        placesRepository = new PlacesRepositoryImpl(placesApiService);
+        restaurantsRepository = new RestaurantsRepositoryImpl(placesApiService);
     }
 
     @Test
-    public void get_nearby_places_should_not_be_null_or_empty() {
-        List<Place> places = TestUtils.getValueForTesting(placesRepository.getNearbyPlaceResponse(defaultLocation));
+    public void get_near_restaurants_should_not_be_null_with_parameter_location() {
+        List<Restaurant> restaurants = TestUtils.getValueForTesting(restaurantsRepository.getNearRestaurants(defaultLocation));
 
-        assertNotNull(places);
+        assertNotNull(restaurants);
     }
 
     @Test
-    public void get_nearby_places_should_be_null_with_empty_parameter() {
-        List<Place> places = TestUtils.getValueForTesting(placesRepository.getNearbyPlaceResponse(""));
+    public void get_near_restaurants_should_be_null_with_empty_parameter() {
+        List<Restaurant> restaurants = TestUtils.getValueForTesting(restaurantsRepository.getNearRestaurants(""));
 
-        assertNull(places);
+        assertNull(restaurants);
     }
 }
