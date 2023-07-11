c714173a1885067c4bb04cbf1aecacee8b479a04
Ken <ken.su_dev@hotmail.com>
2023-07-11 15:49:49 +0200

    Add landscape UI

--- app/src/main/java/com/suonk/oc_project7/ui/workmates/WorkmatesListAdapter.java
+++ app/src/main/java/com/suonk/oc_project7/ui/workmates/WorkmatesListAdapter.java
@@ -1,6 +1,7 @@
 package com.suonk.oc_project7.ui.workmates;
 
 import android.annotation.SuppressLint;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
